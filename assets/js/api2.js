42d6b60215e4e27e3ca9d7b69cf8af4a219592e4
bolttexturas <137934167+victorndesigner@users.noreply.github.com>
2025-02-26 18:31:18 -0300

    att

--- assets/js/api2.js
+++ assets/js/api2.js
@@ -6,21 +6,21 @@ function getPosts() {
             title: "SemTextura",
             version: "0.83.6",
             image: "assets/imagens/REVIVER00.png",
-            steam: "https://sannerurl.com/rvv-stext-st",
+            steam: "https://sannerurl.com/rvv-stext-ste",
             mobile: "https://sannerurl.com/rvv-stxt-mob"
         },
         {
             title: "MegaLite",
             version: "0.83.6",
             image: "assets/imagens/REVIVER01.png",
-            steam: "https://sannerurl.com/rvv-mglt-st",
+            steam: "https://sannerurl.com/rvv-mglt-ste",
             mobile: "https://sannerurl.com/rvv-mglt-mob"
         },
         {
             title: "MegaLite2",
             version: "0.83.6",
             image: "assets/imagens/REVIVER02.png",
-            steam: "https://sannerurl.com/rvv-mglt2-st",
+            steam: "https://sannerurl.com/rvv-mglt2-ste",
             mobile: "https://sannerurl.com/rvv-mglt2-mob"
         },
         {
