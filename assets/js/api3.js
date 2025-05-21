bf63a0625d670775d626c7e2c653a7eed97eb38b
bolttexturas <137934167+victorndesigner@users.noreply.github.com>
2025-05-21 06:16:03 -0300

    att

--- assets/js/api3.js
+++ assets/js/api3.js
@@ -1,132 +1,132 @@
-const scriptVersion = "0.80";
+const scriptVersion = "0.56";
 
 function getPosts() {
     return [
         {
             title: "MegaLite",
-            version: "0.84",
-            image: "assets/imagens/MEGALITEBOLTASTAX.png",
-            steam: "https://sannerurl.com/txt-mglt-ste",
-            mobile: "https://sannerurl.com/txt-mglt-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS01.png",
+            steam: "https://sannerurl.com/cups-mglt-st",
+            mobile: "https://sannerurl.com/cups-mglt-mo"
         },
         {
             title: "MegaLite2",
-            version: "0.84",
-            image: "assets/imagens/MEGALITE2.0BOLTASTAX.png",
-            steam: "https://sannerurl.com/txt-mglt2-ste",
-            mobile: "https://sannerurl.com/txt-mglt2-mobi"
+            version: "0.50",
+            image: "assets/imagens/CUPS02.png",
+            steam: "https://sannerurl.com/cups-mglt2-st",
+            mobile: "https://sannerurl.com/cups-mglt2-mo"
         },
         {
             title: "StumbleBolt",
-            version: "0.84",
-            image: "assets/imagens/STUMBLEBOLT.png",
-            steam: "https://sannerurl.com/txt-stb-ste",
-            mobile: "https://sannerurl.com/txt-stb-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS03.png",
+            steam: "https://sannerurl.com/cups-stb-st",
+            mobile: "https://sannerurl.com/cups-stb-mo"
         },
         {
             title: "StumbleBolt2",
-            version: "0.84",
-            image: "assets/imagens/STUMBLEBOLT2.0.png",
-            steam: "https://sannerurl.com/txt-stb2-ste",
-            mobile: "https://sannerurl.com/txt-stb2-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS04.png",
+            steam: "https://sannerurl.com/cups-stb2-st",
+            mobile: "https://sannerurl.com/cups-stb2-mo"
         },
         {
             title: "StumbleGodz",
-            version: "0.84",
-            image: "assets/imagens/STUMBLEGODZ.png",
-            steam: "https://sannerurl.com/txt-godz-ste",
-            mobile: "https://sannerurl.com/txt-godz-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS05.png",
+            steam: "https://sannerurl.com/cups-godz-st",
+            mobile: "https://sannerurl.com/cups-godz-mo"
         },
         {
             title: "StumbleGodzExc",
-            version: "0.84",
-            image: "assets/imagens/STUMBLEGODZEXC.png",
-            steam: "https://sannerurl.com/txt-godzex-ste",
-            mobile: "https://sannerurl.com/txt-godzex-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS06.png",
+            steam: "https://sannerurl.com/cups-godzex-st",
+            mobile: "https://sannerurl.com/cups-godzex-mo"
         },
         {
             title: "GotiGreen",
-            version: "0.84",
-            image: "assets/imagens/GOTIGREEN.png",
-            steam: "https://sannerurl.com/txt-goti-ste",
-            mobile: "https://sannerurl.com/txt-goti-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS07.png",
+            steam: "https://sannerurl.com/cups-goti-st",
+            mobile: "https://sannerurl.com/cups-goti-mo"
         },
         {
             title: "GotiCartoon",
-            version: "0.84",
-            image: "assets/imagens/GOTICARTOON.png",
-            steam: "https://sannerurl.com/txt-toon-ste",
-            mobile: "https://sannerurl.com/txt-toon-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS18.png",
+            steam: "https://sannerurl.com/cups-toon-st",
+            mobile: "https://sannerurl.com/cups-toon-mo"
         },
         {
             title: "Yellow Otimizada",
-            version: "0.84",
-            image: "assets/imagens/YELLOWOTIMIZADA.png",
-            steam: "https://sannerurl.com/txt-ylw-ste",
-            mobile: "https://sannerurl.com/txt-ylw-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS08.png",
+            steam: "https://sannerurl.com/cups-ylw-st",
+            mobile: "https://sannerurl.com/cups-ylw-mo"
         },
         {
             title: "MegaLite Reshade",
-            version: "0.84",
-            image: "assets/imagens/RESHADERBOLTASTAX.png",
-            steam: "https://sannerurl.com/txt-mgltre-ste",
-            mobile: "https://sannerurl.com/txt-mgltre-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS09.png",
+            steam: "https://sannerurl.com/cups-mgltre-st",
+            mobile: "https://sannerurl.com/cups-mgltre-mo"
         },
         {
             title: "Reshade",
-            version: "0.84",
-            image: "assets/imagens/RESHADER.png",
-            steam: "https://sannerurl.com/txt-re-ste",
-            mobile: "https://sannerurl.com/txt-re-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS10.png",
+            steam: "https://sannerurl.com/cups-re-st",
+            mobile: "https://sannerurl.com/cups-re-mo"
         },
         {
             title: "StumblePink",
-            version: "0.84",
-            image: "assets/imagens/STUMBLEPINK.png",
-            steam: "https://sannerurl.com/txt-pink-ste",
-            mobile: "https://sannerurl.com/txt-pink-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS11.png",
+            steam: "https://sannerurl.com/cups-pink-st",
+            mobile: "https://sannerurl.com/cups-pink-mo"
         },
         {
             title: "BoltAgua",
-            version: "0.84",
-            image: "assets/imagens/BOLTAGUA.png",
-            steam: "https://sannerurl.com/txt-agua-ste",
-            mobile: "https://sannerurl.com/txt-agua-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS12.png",
+            steam: "https://sannerurl.com/cups-agua-st",
+            mobile: "https://sannerurl.com/cups-agua-mo"
         },
         {
             title: "BoltAstaxzz",
-            version: "0.84",
-            image: "assets/imagens/BOLTAXZZ.png",
-            steam: "https://sannerurl.com/txt-baz-ste",
-            mobile: "https://sannerurl.com/txt-baz-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS13.png",
+            steam: "https://sannerurl.com/cups-baz-st",
+            mobile: "https://sannerurl.com/cups-baz-mo"
         },
         {
             title: "DarkUva",
-            version: "0.84",
-            image: "assets/imagens/DARKUVA.png",
-            steam: "https://sannerurl.com/txt-uva-ste",
-            mobile: "https://sannerurl.com/txt-uva-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS14.png",
+            steam: "https://sannerurl.com/cups-uva-st",
+            mobile: "https://sannerurl.com/cups-uva-mo"
         },
         {
             title: "BoltWeen",
-            version: "0.84",
-            image: "assets/imagens/BOLTWEEN.png",
-            steam: "https://sannerurl.com/txt-ween-ste",
-            mobile: "https://sannerurl.com/txt-ween-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS15.png",
+            steam: "https://sannerurl.com/cups-ween-st",
+            mobile: "https://sannerurl.com/cups-ween-mo"
         },
         {
             title: "Galaxy1",
-            version: "0.84",
-            image: "assets/imagens/GALAXY1.png",
-            steam: "https://sannerurl.com/txt-glxk-ste",
-            mobile: "https://sannerurl.com/txt-glxk-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS16.png",
+            steam: "https://sannerurl.com/cups-glxk-st",
+            mobile: "https://sannerurl.com/cups-glxk-mo"
         },
         {
             title: "GalaxyBolt",
-            version: "0.84",
-            image: "assets/imagens/GALAXYBOLT.png",
-            steam: "https://sannerurl.com/txt-glxb-ste",
-            mobile: "https://sannerurl.com/txt-glxb-mob"
+            version: "0.50",
+            image: "assets/imagens/CUPS17.png",
+            steam: "https://sannerurl.com/cups-glxb-st",
+            mobile: "https://sannerurl.com/cups-glxb-mo"
         }
     ];
 }
@@ -147,7 +147,7 @@ function renderPosts(posts) {
                         </span>
                         <h4>${postagem.title}<br><span style="color: rgb(208, 190, 233);">${postagem.version}</span></h4>
                         <div class="button-container">
-                            <a href="${postagem.steam}" class="purple-button">Steam</a>
+                            <!--<a href="${postagem.steam}" class="purple-button">Steam</a>-->
                             <a href="${postagem.mobile}" class="purple-button">Mobile</a>
                         </div>
                     </div>
@@ -160,7 +160,7 @@ function renderPosts(posts) {
                         <span style="pointer-events: none;">
                             <img src="${postagem.image}" alt="${postagem.title}">
                         </span>
-                        <h4 style="color: gray;">${postagem.title}<br><span>Desatualizada</span></h4>
+                        <h4 style="color: gray;">${postagem.title}<br><span>Em Breve...</span></h4>
                     </div>
                 </div>
             `);
